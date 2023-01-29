c885c012d47736d92aff5001fb536c48dd646ec6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-29 03:02:08 +0100

    fix: add card component

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,17 +1,18 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title='Reset' />
           <Button title='Confirm' />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -30,14 +31,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 5,
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
