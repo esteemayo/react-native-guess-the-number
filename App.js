97ed668a10a3bd95962869801cfb49bb4e9fffb5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-29 22:08:38 +0100

    fix: modify

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
+import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
-      <StartGameScreen />
+      {content}
       <StatusBar style="auto" />
     </View>
   );
