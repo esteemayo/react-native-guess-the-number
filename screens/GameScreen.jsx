71f1a6429213f27586a208247a1616dd7ada5e9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-29 18:18:08 +0100

    feat: create game screen

---
+++ screens/GameScreen.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import Card from '../components/Card';
+import NumberContainer from '../components/NumberContainer';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude)
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = ({ userChoice }) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title='LOWER' />
+        <Button title='GREATER' />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%',
+  },
+});
+
+export default GameScreen;
