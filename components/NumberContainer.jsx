bbfe6973637264bba85903ed48fe26a352ded612
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-29 17:39:53 +0100

    feat: add number container component

---
+++ components/NumberContainer.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Colors from '../constants/color';
+
+const NumberContainer = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.number}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    borderColor: Colors.accent,
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  number: {
+    color: Colors.accent,
+    fontSize: 22,
+  },
+});
+
+export default NumberContainer;
