b0254f41d6732bdbe472af6f48fcd23be388636d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-29 04:07:05 +0100

    fix: modify

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,16 +1,32 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/color';
 
 const StartGameScreen = () => {
+  const [number, setNumber] = useState('');
+
+  const handleChange = (value) => {
+    setNumber(value.replace(/[^0-9]/g, ''));
+  };
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <Input style={styles.input} />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='number-pad'
+          maxLength={2}
+          value={number}
+          onChangeText={handleChange}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title='Reset' color={Colors.accent} />
