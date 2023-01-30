9abb4993453c2a71be062fed2e0a4ce1e046c5e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-30 08:38:16 +0100

    fix: modify

--- App.js
+++ App.js
@@ -4,19 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />
   }
 
   return (
