35b0b8297e16293114212e11656ac01c918cf15e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-30 08:29:50 +0100

    fix: add useEffect hook

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 import Card from '../components/Card';
