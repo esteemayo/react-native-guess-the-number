59461c121119855b4dd24228f5642daf101b066a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-30 08:15:44 +0100

    feat: create game over screen

---
+++ screens/GameOverScreen.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const GameOverScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is Over!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default GameOverScreen;
